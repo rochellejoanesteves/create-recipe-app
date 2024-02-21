274e665c505407b04372235b99e7bc05a85b7ead
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-02-22 07:45:50 +0800

    add create recipe form

--- src/pages/create/index.jsx
+++ src/pages/create/index.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import "./create.scss"
+import React, { useState } from "react";
+import "./create.scss";
 
 const Create = () => {
-  return (
-    <div>
-      Create
-    </div>
-  )
-}
+  const [recipe, setRecipe] = useState({});
 
-export default Create
+  const handleChange = (e) => {
+    setRecipe({ ...recipe, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Recipe</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title: </span>
+          <input type="text" id="title" onChange={handleChange} required />
+        </label>
+        <label>
+          <span>Recipe Method: </span>
+          <textarea type="text" id="method" onChange={handleChange} required />
+        </label>
+        <label>
+          <span>Cooking Time (minutes): </span>
+          <input
+            type="number"
+            id="cookingTime"
+            onChange={handleChange}
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Create;
