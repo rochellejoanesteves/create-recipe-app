cb16359d279dfd3f06eeba6404b4d3d3788c642f
Rochelle <rochellejoan.p.esteves@gmail.com>
2024-02-21 17:14:23 +0800

    fetch recipes data

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import "./home.scss"
+import React, { useEffect, useState } from "react";
+import "./home.scss";
 
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-export default Home
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        setError(false);
+        setLoading(true);
+        const res = await fetch("http://localhost:3006/recipes");
+        const data = await res.json();
+        setRecipes(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+      }
+    };
+    fetchRecipe();
+  }, []);
+
+  return (
+    <div className="home">
+      {error && <p className="error">Error fetching data...</p>}
+      {loading && <p className="loading">Loading...</p>}
+      {recipes &&
+        recipes.map((recipe) => (
+          <div key={recipe.id}>
+            <h2>{recipe.title}</h2>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Home;
